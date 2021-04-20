f1a6f95120dab5d4c444e5142308ad4cfbceaedd
megan cho <meganlisacho@gmail.com>
2021-04-19 22:29:40 -0500

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+const titleCased = (tutorials) => {
+  return tutorials.map(tutorial => tutorial.split(' '))
+  .map(tutorial => tutorial[0].charAt(0).toUpperCase() + tutorial.slice(1))
+  .join(' ')
 }
+
